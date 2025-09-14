f51154d76c34316dec97e976c57bc1034b348051
yokamoto5742-h <yokamoto5742@gmail.com>
2025-09-14 12:26:12 +0900

    ✨ feat(VertexAI_API.py): Google APIクライアント設定を更新し、場所とAPIバージョンを追加

--- VertexAI_API.py
+++ VertexAI_API.py
@@ -1,9 +1,11 @@
 from google import genai
+from google.genai import types
 
 client = genai.Client(
     vertexai=True,
-    project="gen-lang-client-0605794434",
-    location="asia-northeast1",
+    project='gen-lang-client-0605794434',
+    location='global',
+    http_options=types.HttpOptions(api_version='v1')
 )
 
 response = client.models.generate_content(
