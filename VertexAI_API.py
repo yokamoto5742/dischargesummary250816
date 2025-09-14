45a14239d72cac9b8acff3cbfe3e0c4ceb48aa9a
yokamoto5742-h <yokamoto5742@gmail.com>
2025-09-14 13:37:32 +0900

    ✨ feat(VertexAI_API.py): APIクライアントの設定とモデル呼び出しを追加

--- VertexAI_API.py
+++ VertexAI_API.py
@@ -1,15 +1,13 @@
 from google import genai
-from google.genai import types
 
 client = genai.Client(
     vertexai=True,
     project='gen-lang-client-0605794434',
-    location='global',
-    http_options=types.HttpOptions(api_version='v1')
+    location='asia-northeast1',
 )
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="How does AI work?",
+    contents="何かジョークを言ってください",
 )
 print(response.text)
